b4c51f9727125203a418d38e54fea0a9944600eb
gomes-leonardo <leonardo.r.gomes@hotmail.com>
2025-11-25 23:39:56 -0300

    chore: remove test to front end

--- __tests__/mocks/server.ts
+++
@@ -1,8 +0,0 @@
-import { setupServer } from "msw/node";
-import { handlers } from "./handlers";
-
-/**
- * MSW Server para mockar chamadas de API em testes
- * Simula o comportamento do backend Node + Express
- */
-export const server = setupServer(...handlers);
